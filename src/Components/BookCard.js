f78993caa07f7fde0dae1b251abf3d2f3bcd8a14
Cat Front Page <chiangjason19@gmail.com>
2019-02-23 21:04:23 -0800

    Initial commit

---
+++ src/Components/BookCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BookCard = (props) => {
+
+    return(
+       <a href={props.infoLink} target='_blank' rel='noopener noreferrer' className="card-container">
+            <img src={props.image} alt="" className="book-image"/>
+            <div className="description">
+                <h2 className="description-title">{props.title}</h2>
+                <h3 className="description-author">Author: {props.author}</h3>
+                <h3 className="description-publisher">Published By: {props.published}</h3>
+            </div>
+       </a>
+    )
+}
+
+export default BookCard;
